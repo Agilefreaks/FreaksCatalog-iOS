c12c0c7807f7dc92b3782e34c9681ed6b09740c4
DejanuTeodora <theo_adry_20@yahoo.com>
2021-07-20 10:56:12 +0300

    Setup project

---
+++ Shared/FreaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreaksApp.swift
+//  Shared
+//
+//  Created by Teodora Dejanu on 20.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct FreaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

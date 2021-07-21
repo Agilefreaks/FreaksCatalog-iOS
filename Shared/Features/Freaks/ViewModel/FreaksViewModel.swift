5c36a522a6a3494afd07985f5210ed62f656ed3c
Radu Moldovan <moldovan998@gmail.com>
2021-07-21 11:33:44 +0300

    Implement MVVM Architecture

---
+++ Shared/Features/Freaks/ViewModel/FreaksViewModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+final class FreaksViewModel {
+    var freaks: [Freak] = [
+        Freak(imageName: "person.crop.square", firstName: "Vlad", lastName: "Stanescu"),
+        Freak(imageName: "person.crop.square", firstName: "Mihai", lastName: "Pantiru"),
+        Freak(imageName: "person.crop.square", firstName: "Raluca", lastName: "Ionescu"),
+    ]
+}

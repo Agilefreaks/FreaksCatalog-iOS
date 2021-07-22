3ebbc91e6618851f5b5fd724fb4b1a3b6aa33eec
DejanuTeodora <theo_adry_20@yahoo.com>
2021-07-22 16:24:49 +0300

    Add Skills & Projects buttons

--- Shared/Features/Freaks/View/FreaksView.swift
+++ Shared/Features/Freaks/View/FreaksView.swift
@@ -7,16 +7,35 @@ struct FreaksView: View {
         let coloredNavAppearance = UINavigationBarAppearance()
         coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color("SecondaryColor"))]
         coloredNavAppearance.backgroundColor = UIColor(Color("AccentColor"))
-        
+
         UINavigationBar.appearance().standardAppearance = coloredNavAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
     }
 
     var body: some View {
         NavigationView {
-            List(viewModel.freaks, id: \.firstName) { freak in
-                NavigationLink(destination: FreakDetailsView(viewModel: FreakDetailsViewModel(freak: freak))) {
-                    ListItemView(viewModel: ListItemViewModel(freak: freak))
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    Button("Skills") {
+                        print("skills")
+                    }
+                    Spacer()
+                    Button("Projects") {
+                        print("projects")
+                    }
+                    Spacer()
+                }
+                .frame(height: 60)
+                .foregroundColor(.white)
+                .font(.title3)
+                .background(Color("AccentColor"))
+                .border(Color("SecondaryColor"), width: 6)
+
+                List(viewModel.freaks, id: \.firstName) { freak in
+                    NavigationLink(destination: FreakDetailsView(viewModel: FreakDetailsViewModel(freak: freak))) {
+                        ListItemView(viewModel: ListItemViewModel(freak: freak))
+                    }
                 }
             }
             .navigationBarTitle("Freaks", displayMode: .inline)

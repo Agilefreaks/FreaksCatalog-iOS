0c52a8081333d35d0fcefc0d2052d3c17777051a
Radu Moldovan <moldovan998@gmail.com>
2021-07-26 14:20:03 +0300

    Put extension in different file

--- Shared/Features/FreakDetails/View/FreakDetailsView.swift
+++ Shared/Features/FreakDetails/View/FreakDetailsView.swift
@@ -1,17 +1,5 @@
 import SwiftUI
 
-// Get back swipe left gesture after hiding back button
-extension UINavigationController: UIGestureRecognizerDelegate {
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
-
-    public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
-        viewControllers.count > 1
-    }
-}
-
 struct FreakDetailsView: View {
     @Environment(\.presentationMode) var presentationMode
     var viewModel: FreakDetailsViewModel
---
+++ Shared/Utils/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+// Get back swipe left gesture after hiding back button
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}

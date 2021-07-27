bed9134379ed2437b1c995517437aaf1546e427e
DejanuTeodora <theo_adry_20@yahoo.com>
2021-07-27 11:17:50 +0300

    Add details

--- Shared/Features/FreakDetails/View/FreakDetailsView.swift
+++ Shared/Features/FreakDetails/View/FreakDetailsView.swift
@@ -5,14 +5,28 @@ struct FreakDetailsView: View {
     var viewModel: FreakDetailsViewModel
 
     var body: some View {
-        Text(viewModel.fullName)
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
-                HStack {
-                    Image(systemName: "chevron.left")
-                    Text(viewModel.fullName)
-                }
-            })
+        ZStack {
+            Rectangle()
+                .fill(Color("AccentColor"))
+                .frame(maxWidth: 300, maxHeight: 400, alignment: .center)
+
+            VStack {
+                Image(viewModel.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: 160, maxHeight: 160)
+                    .cornerRadius(10)
+                    .accessibility(identifier: "freakImage")
+                Text(viewModel.fullName)
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                            Text(viewModel.fullName)
+                        }
+                    })
+            }
+        }
     }
 }
 

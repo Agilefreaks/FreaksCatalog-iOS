53af7deb2b66113afd4bd99bb065c176b457062d
DejanuTeodora <theo_adry_20@yahoo.com>
2021-07-28 09:20:30 +0300

    Resize text.Draw a border around freak image

--- Shared/Features/FreakDetails/View/FreakDetailsView.swift
+++ Shared/Features/FreakDetails/View/FreakDetailsView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct FreakDetailsView: View {
     @Environment(\.presentationMode) var presentationMode
     var viewModel: FreakDetailsViewModel
-    // let freak = Bundle.main.decode([Freak].self, from: "MockData.json")
 
     var body: some View {
         ZStack {
@@ -19,20 +18,21 @@ struct FreakDetailsView: View {
                     .aspectRatio(contentMode: .fill)
                     .frame(maxWidth: 160, maxHeight: 160)
                     .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color("SecondaryColor"), lineWidth: 2)
+                    )
                     .accessibility(identifier: "freakImage")
                 Text(viewModel.fullName)
-                    .navigationBarBackButtonHidden(true)
-                    .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
-                        HStack {
-                            Image(systemName: "chevron.left")
-                            Text(viewModel.fullName)
-                        }
-                    })
-
-                Text(viewModel.description)
+                    .font(.system(size: 18, weight: .bold, design: .default))
                     .padding()
 
-                VStack(alignment: .leading, spacing: 2) {
+                Text(viewModel.description)
+                    .font(.system(size: 16, weight: .medium, design: .default))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+
+                VStack(alignment: .leading, spacing: 15) {
                     HStack {
                         Text("Skills: ")
                         ForEach(viewModel.skills, id: \.self) { skill in
@@ -52,16 +52,27 @@ struct FreakDetailsView: View {
                         Text(viewModel.level)
                     }
                 }
-            }
 
+                .foregroundColor(Color("SecondaryColor"))
+                .font(.system(size: 18, weight: .medium, design: .default))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 40)
+            }
             .frame(maxWidth: 350, maxHeight: 650, alignment: .top)
+            .foregroundColor(Color("SecondaryColor"))
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                Text(viewModel.fullName)
+            }
+        })
     }
 }
 
 struct FreakDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        FreakDetailsView(viewModel: FreakDetailsViewModel(freak: Freak(imageName: "test1", firstName: "Vlad", lastName: "Stanescu", description: "Lorem ipsum", skills: ["iOS"], projects: ["Epix"], role: "iOS Developer", level: "Senior", norm: "Fulltime", id: 0)))
-            .previewInterfaceOrientation(.portraitUpsideDown)
+        FreakDetailsView(viewModel: FreakDetailsViewModel(freak: Freak(imageName: "test1", firstName: "Vlad", lastName: "Stanescu", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae egestas enim. Ut vel orci interdum, egestas orci id, molestie urna. Phasellus sit amet ipsum nec urna efficitur dapibus a sit amet nunc.", skills: ["iOS"], projects: ["Epix"], role: "iOS Developer", level: "Senior", norm: "Fulltime", id: 0)))
     }
 }

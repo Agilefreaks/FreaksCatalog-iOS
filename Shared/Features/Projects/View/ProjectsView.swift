f64e4a8b6e94fe92adaef7587373b65e96ef9ec1
Radu Moldovan <moldovan998@gmail.com>
2021-08-16 14:27:56 +0300

    Add navbar

--- Shared/Features/Projects/View/ProjectsView.swift
+++ Shared/Features/Projects/View/ProjectsView.swift
@@ -1,8 +1,28 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    init() {
+        let coloredNavAppearance = UINavigationBarAppearance()
+        coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color("SecondaryColor"))]
+        coloredNavAppearance.backgroundColor = UIColor(Color("AccentColor"))
+
+        UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
+    }
+
     var body: some View {
-        Text("Projects")
+        NavigationView {
+            Text("Projects")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("Projects")
+                            .fontWeight(.medium)
+                            .font(.system(size: 24))
+                            .foregroundColor(Color("SecondaryColor"))
+                    }
+                }
+        }
     }
 }
 

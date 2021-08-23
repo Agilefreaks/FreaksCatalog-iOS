bb82f7b04bb451b97629d1478bfa3db998bdde13
DejanuTeodora <68540003+DejanuTeodora@users.noreply.github.com>
2021-08-23 16:05:30 +0300

    Fix UI tests (#36)

--- Shared/Features/Menu/View/MenuView.swift
+++ Shared/Features/Menu/View/MenuView.swift
@@ -1,20 +1,27 @@
 import SwiftUI
 
 struct MenuView: View {
+    @State private var selection = 2
+
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
             HomeView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
+                .tag(1)
+
             FreaksView()
                 .tabItem {
                     Label("Freaks", systemImage: "person.3.fill")
                 }
+                .tag(2)
+
             ProjectsView()
                 .tabItem {
                     Label("Projects", systemImage: "square.stack.3d.up.fill")
                 }
+                .tag(3)
         }
         .accentColor(Color("AccentColor"))
     }
